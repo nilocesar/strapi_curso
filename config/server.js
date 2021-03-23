f06e9bff0829540ee02373ad37107db08bbad2a8
Nilo César <e_maildonilo@yahoo.com.br>
2021-03-23 18:41:56 -0300

    strapi curso

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '740697bc3b21cd10b50d888760a5b106'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd136c0b0-8944-4076-8ad0-2a37a850ebb5'
+};
